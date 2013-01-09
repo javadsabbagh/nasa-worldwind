d3a35b5324db1ec979a298131fc388498151aa44
pabercrombie <pabercrombie@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2013-01-09 19:57:49 +0000

    Added unit tests to ensure that OGLUtil.newTextureData supports indexed color and interlaced PNG images.

--- branches/2.0.0/WorldWind/test/gov/nasa/worldwind/util/OGLUtilTest.java
+++ branches/2.0.0/WorldWind/test/gov/nasa/worldwind/util/OGLUtilTest.java
@@ -6,11 +6,12 @@
 
 package gov.nasa.worldwind.util;
 
-import com.jogamp.opengl.util.texture.TextureData;
+import com.jogamp.opengl.util.texture.*;
 import gov.nasa.worldwind.Configuration;
 import junit.framework.TestCase;
 import org.junit.Test;
 
+import javax.media.opengl.GL;
 import java.io.*;
 import java.net.URL;
 
@@ -126,5 +127,27 @@ public class OGLUtilTest
 
         TestCase.assertEquals(td.getWidth(), 2048);
         TestCase.assertEquals(td.getHeight(), 1024);
+    }
+
+    /** Test that the newTextureData supports indexed color PNG images. See http://issues.worldwind.arc.nasa.gov/jira/browse/WWJ-369. */
+    @Test
+    public void testIndexedColorPng() throws IOException
+    {
+        URL url = new File("./testData/32x32-icon-nasa-indexed-color.png").toURI().toURL();
+
+        TextureData td = OGLUtil.newTextureData(Configuration.getMaxCompatibleGLProfile(), url, false);
+        TestCase.assertEquals(td.getPixelFormat(), GL.GL_RGBA);
+    }
+
+    /** Test that the newTextureData supports interlaced PNG images. See http://issues.worldwind.arc.nasa.gov/jira/browse/WWJ-365. */
+    @Test
+    public void testInterlacedPng() throws IOException
+    {
+        URL url = new File("./testData/32x32-icon-nasa-interlaced.png").toURI().toURL();
+
+        TextureData td = OGLUtil.newTextureData(Configuration.getMaxCompatibleGLProfile(), url, false);
+
+        TestCase.assertEquals(td.getWidth(), 32);
+        TestCase.assertEquals(td.getHeight(), 32);
     }
 }
