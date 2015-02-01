ec1ae16abaf2039bce5b9a5784f0ef8365376a46
tgaskins <tgaskins@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2015-02-01 21:44:40 +0000

    Documented the Placemarks example.

--- trunk/WebWorldWind/examples/Placemarks.js
+++ trunk/WebWorldWind/examples/Placemarks.js
@@ -3,6 +3,8 @@
  * National Aeronautics and Space Administration. All Rights Reserved.
  */
 /**
+ * Illustrates how to display and pick Placemarks.
+ *
  * @version $Id$
  */
 
@@ -12,12 +14,19 @@ requirejs(['../src/WorldWind',
               LayerManager) {
         "use strict";
 
+        // Tell World Wind to log only warnings.
         WorldWind.Logger.setLoggingLevel(WorldWind.Logger.LEVEL_WARNING);
 
+        // Create the World Window.
         var wwd = new WorldWind.WorldWindow("canvasOne");
-        wwd.addLayer(new WorldWind.BMNGLandsatLayer());
-        wwd.addLayer(new WorldWind.BingWMSLayer());
 
+        /**
+         * Added imagery layers.
+         */
+        wwd.addLayer(new WorldWind.BMNGLandsatLayer()); // Blue Marble + Landsat
+        wwd.addLayer(new WorldWind.BingWMSLayer()); // Bing
+
+        // Define the images we'll use for the placemarks.
         var images = [
             "plain-black.png",
             "plain-blue.png",
@@ -42,14 +51,15 @@ requirejs(['../src/WorldWind',
             "castshadow-white.png"
         ];
 
-        var pinLibrary = "http://worldwindserver.net/webworldwind/images/pushpins/",
+        var pinLibrary = "http://worldwindserver.net/webworldwind/images/pushpins/", // location of the image files
             placemark,
             placemarkAttributes = new WorldWind.PlacemarkAttributes(null),
             highlightAttributes,
-            placemarkLayer = new WorldWind.RenderableLayer(),
+            placemarkLayer = new WorldWind.RenderableLayer("Placemarks"),
             latitude = 47.684444,
             longitude = -122.129722;
 
+        // Set up the common placemark attributes.
         placemarkAttributes.imageScale = 1;
         placemarkAttributes.imageOffset = new WorldWind.Offset(
             WorldWind.OFFSET_FRACTION, 0.5,
@@ -60,61 +70,87 @@ requirejs(['../src/WorldWind',
             WorldWind.OFFSET_FRACTION, 1.0);
         placemarkAttributes.labelAttributes.color = WorldWind.Color.YELLOW;
 
+        // For each placemark image, create a placemark with a label.
         for (var i = 0, len = images.length; i < len; i++) {
+            // Create the placemark and its label.
             placemark = new WorldWind.Placemark(new WorldWind.Position(latitude, longitude + i, 1e2));
             placemark.label = "Placemark " + i.toString() + "\n"
             + "Lat " + latitude.toPrecision(4).toString() + "\n"
             + "Lon " + longitude.toPrecision(5).toString();
+
+            // Create the placemark attributes for this placemark. Note that the attributes differ only by their
+            // image URL.
             placemarkAttributes = new WorldWind.PlacemarkAttributes(placemarkAttributes);
             placemarkAttributes.imagePath = pinLibrary + images[i];
+            placemark.attributes = placemarkAttributes;
+
+            // Create the highlight attributes for this placemark. Note that the normal attributes are specified as
+            // the default highlight attributes so that all properties are identical except the image scale. You could
+            // instead vary the color, image, or other property to control the highlight representation.
             highlightAttributes = new WorldWind.PlacemarkAttributes(placemarkAttributes);
             highlightAttributes.imageScale = 1.2;
-            placemark.attributes = placemarkAttributes;
             placemark.highlightAttributes = highlightAttributes;
+
+            // Add the placemark to the layer.
             placemarkLayer.addRenderable(placemark);
         }
 
-        placemarkLayer.displayName = "Placemarks";
+        // Add the placemarks layer to the World Window's layer list.
         wwd.addLayer(placemarkLayer);
 
+        // Draw the World Window for the first time.
         wwd.redraw();
 
+        // Create a layer manager for controlling layer visibility.
         var layerManger = new LayerManager('divLayerManager', wwd);
+
+        // Now set up to handle picking.
 
         var canvas = document.getElementById("canvasOne"),
             highlightedItems = [];
 
+        // Listen for mouse moves and highlight the placemarks that the cursor rolls over.
         canvas.addEventListener("mousemove", function (e) {
             handlePick(e.clientX, e.clientY);
         }, false);
 
+        // Listen for taps on mobile devices and highlight the placemarks that the user taps.
         var tapRecognizer = new WorldWind.TapRecognizer(canvas);
         tapRecognizer.addGestureListener(function (recognizer) {
             var location = recognizer.location();
             handlePick(location[0], location[1]);
         });
 
+        // The common pick-handling function.
         var handlePick = function (x, y) {
-            var redrawRequired = highlightedItems.length > 0;
+            var redrawRequired = highlightedItems.length > 0; // must redraw if we de-highlight previously picked items
 
-            // De-highlight any highlighted placemarks.
+            // De-highlight any previously highlighted placemarks.
             for (var h = 0; h < highlightedItems.length; h++) {
                 highlightedItems[h].highlighted = false;
             }
             highlightedItems = [];
 
-            // Perform the pick.
+            // Perform the pick. Must first convert from canvas coordinates to World Window viewport coordinates.
+            // Canvas coordinates have an upper-right origin. World Window viewport coordinates have a lower-left
+            // origin.
             var pickList = wwd.pick(wwd.canvasCoordinates(x, y));
             if (pickList.objects.length > 0) {
                 redrawRequired = true;
             }
 
-            // Highlight the items picked.
+            // Highlight the items picked by simply setting their highlight flag to true.
             if (pickList.objects.length > 0) {
                 for (var p = 0; p < pickList.objects.length; p++) {
                     pickList.objects[p].userObject.highlighted = true;
+
+                    // Keep track of highlighted items in order to de-highlight them later.
                     highlightedItems.push(pickList.objects[p].userObject);
 
+                    // Detect whether the placemark's label was picked. If so, the "labelPicked" property is true.
+                    // If instead the user picked the placemark's image, the "labelPicked" property is false.
+                    // Applications might use this information to determine whether the user wants to edit the label
+                    // or is merely picking the placemark as a whole.
                     if (pickList.objects[p].labelPicked) {
                         console.log("Label picked");
                     }
