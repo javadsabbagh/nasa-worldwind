5febd2d1bbb303f1d8d25fca3593fdf9a19e67fb
tgaskins <tgaskins@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2015-02-02 00:40:03 +0000

    Added pushpin images to source and updated examples to draw them from the current origin.

--- WebWorldWind/examples/PlacemarksAndPicking.js
+++ WebWorldWind/examples/PlacemarksAndPicking.js
@@ -51,7 +51,7 @@ requirejs(['../src/WorldWind',
             "castshadow-white.png"
         ];
 
-        var pinLibrary = "http://worldwindserver.net/webworldwind/images/pushpins/", // location of the image files
+        var pinLibrary = WorldWind.WWUtil.currentUrlSansFilePart() + "/../images/pushpins/", // location of the image files
             placemark,
             placemarkAttributes = new WorldWind.PlacemarkAttributes(null),
             highlightAttributes,
