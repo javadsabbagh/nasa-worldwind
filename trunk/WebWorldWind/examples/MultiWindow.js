34796a8f4c5d0775e2abbf224d63dbf9ff8dc1a9
tgaskins <tgaskins@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2015-02-27 19:34:08 +0000

    Modified MultiWindow example to include a shared layer containing a Path.

--- trunk/WebWorldWind/examples/MultiWindow.js
+++ trunk/WebWorldWind/examples/MultiWindow.js
@@ -13,18 +13,44 @@ requirejs(['../src/WorldWind'], function () {
 
     WorldWind.Logger.setLoggingLevel(WorldWind.Logger.LEVEL_WARNING);
 
+    var makePathLayer = function () {
+        var pathAttributes = new WorldWind.PathAttributes(null);
+        pathAttributes.interiorColor = WorldWind.Color.CYAN;
+        pathAttributes.outlineColor= WorldWind.Color.BLUE;
+
+        var pathPositions = [
+            new WorldWind.Position(40, -100, 1e4),
+            new WorldWind.Position(45, -110, 1e4),
+            new WorldWind.Position(46, -122, 1e4)
+        ];
+        var path = new WorldWind.Path(pathPositions);
+        path.attributes = pathAttributes;
+        path.altitudeMode = WorldWind.RELATIVE_TO_GROUND;
+        path.followTerrain = true;
+
+        var pathLayer = new WorldWind.RenderableLayer("Path Layer");
+        pathLayer.addRenderable(path);
+
+        return pathLayer;
+    };
+
+    var pathLayer = makePathLayer();
+
     var wwd1 = new WorldWind.WorldWindow("canvasOne");
     wwd1.addLayer(new WorldWind.BMNGLandsatLayer());
     wwd1.addLayer(new WorldWind.BingWMSLayer());
+    wwd1.addLayer(pathLayer);
     wwd1.redraw();
 
     var wwd2 = new WorldWind.WorldWindow("canvasTwo");
     wwd2.addLayer(new WorldWind.BMNGLandsatLayer());
     wwd2.addLayer(new WorldWind.BingWMSLayer());
+    wwd2.addLayer(pathLayer);
     wwd2.redraw();
 
     var wwd3 = new WorldWind.WorldWindow("canvasThree");
     wwd3.addLayer(new WorldWind.BMNGLandsatLayer());
     wwd3.addLayer(new WorldWind.BingWMSLayer());
+    wwd3.addLayer(pathLayer);
     wwd3.redraw();
 });
