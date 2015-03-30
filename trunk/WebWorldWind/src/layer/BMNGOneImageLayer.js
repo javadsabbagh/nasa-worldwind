b9060949a55508cabf830c0583b3aaacbc159fd0
tgaskins <tgaskins@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2015-03-30 21:16:36 +0000

    Moved BMGNOneImageLayer image to "images" directory.

--- trunk/WebWorldWind/src/layer/BMNGOneImageLayer.js
+++ trunk/WebWorldWind/src/layer/BMNGOneImageLayer.js
@@ -29,7 +29,7 @@ define([
             RenderableLayer.call(this, "Blue Marble Image");
 
             var surfaceImage = new SurfaceImage(Sector.FULL_SPHERE,
-                WWUtil.currentUrlSansFilePart() + "/../src/resources/BMNG_world.topo.bathy.200405.3.2048x1024.jpg");
+                WWUtil.currentUrlSansFilePart() + "/../images/BMNG_world.topo.bathy.200405.3.2048x1024.jpg");
 
             this.addRenderable(surfaceImage);
 
