0c80ac746c15193ddd6054860867447c48652330
tgaskins <tgaskins@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2015-04-28 16:08:48 +0000

    Simplified WWMath.cbrt.

--- WebWorldWind/src/util/WWMath.js
+++ WebWorldWind/src/util/WWMath.js
@@ -57,44 +57,11 @@ define([
              */
             cbrt: function (x) {
                 // Use the built-in version if it exists. cbrt() is defined in ECMA6.
-                if (typeof Math.cbrt == 'function')
+                if (typeof Math.cbrt == 'function') {
                     return Math.cbrt(x);
-
-                // Taken from http://stackoverflow.com/questions/23402414/implementing-an-accurate-cbrt-function-without-extra-precision
-                if (x == 0)
-                    return 0;
-                if (x < 0)
-                    return -WWMath.cbrt(-x);
-
-                var r = x;
-                var ex = 0;
-
-                while (r < 0.125) {
-                    r *= 8;
-                    ex--;
+                } else {
+                    return Math.pow(x, 1/3);
                 }
-                while (r > 1.0) {
-                    r *= 0.125;
-                    ex++;
-                }
-
-                r = (-0.46946116 * r + 1.072302) * r + 0.3812513;
-
-                while (ex < 0) {
-                    r *= 0.5;
-                    ex++;
-                }
-                while (ex > 0) {
-                    r *= 2;
-                    ex--;
-                }
-
-                r = (2.0 / 3.0) * r + (1.0 / 3.0) * x / (r * r);
-                r = (2.0 / 3.0) * r + (1.0 / 3.0) * x / (r * r);
-                r = (2.0 / 3.0) * r + (1.0 / 3.0) * x / (r * r);
-                r = (2.0 / 3.0) * r + (1.0 / 3.0) * x / (r * r);
-
-                return r;
             },
 
             /**
