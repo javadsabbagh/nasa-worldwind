c6c71f16065562292d34e18cdc45ce6e73c968b4
tgaskins <tgaskins@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2015-05-04 21:11:01 +0000

    Initial check-in of Explorer app.

---
+++ trunk/WebWorldWind/apps/Explorer/Explorer.js
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2014 United States Government as represented by the Administrator of the
+ * National Aeronautics and Space Administration. All Rights Reserved.
+ */
+/**
+ * @version $Id$
+ */
+define(['../../src/WorldWind'],
+    function (ww) {
+        "use strict";
+
+        var Explorer = function () {
+            var thisExplorer = this;
+
+            WorldWind.Logger.setLoggingLevel(WorldWind.Logger.LEVEL_WARNING);
+
+            // Create the World Window.
+            this.wwd = new WorldWind.WorldWindow("canvasOne");
+
+            /**
+             * Added imagery layers.
+             */
+            var layers = [
+                {layer: new WorldWind.BMNGLayer(), enabled: true},
+                {layer: new WorldWind.BMNGLandsatLayer(), enabled: false},
+                {layer: new WorldWind.BingAerialWithLabelsLayer(null), enabled: true},
+                {layer: new WorldWind.CompassLayer(), enabled: true},
+                {layer: new WorldWind.ViewControlsLayer(this.wwd), enabled: true}
+            ];
+
+            for (var l = 0; l < layers.length; l++) {
+                layers[l].layer.enabled = layers[l].enabled;
+                this.wwd.addLayer(layers[l].layer);
+            }
+
+            this.wwd.redraw();
+        };
+
+        return Explorer;
+    });
