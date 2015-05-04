75006224555c5156dd31381d9d2b7fb1caf2beb5
tgaskins <tgaskins@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2015-05-04 21:11:01 +0000

    Initial check-in of Explorer app.

---
+++ WebWorldWind/apps/Explorer/app.js
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2014 United States Government as represented by the Administrator of the
+ * National Aeronautics and Space Administration. All Rights Reserved.
+ */
+/**
+ * @version $Id$
+ */
+
+requirejs.config({
+    "paths": {
+        "Explorer": "Explorer"
+    }
+});
+
+requirejs(["Explorer"], function (Explorer) {
+    new Explorer()
+});
