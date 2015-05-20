703953148cbc471cdb94559f3f828c67dc31653d
tgaskins <tgaskins@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2015-05-20 00:20:02 +0000

    Miscellaneous comment changes in examples. Removed BingLayers example since it's now redundant with BasicExample.

--- WebWorldWind/examples/Paths.js
+++ WebWorldWind/examples/Paths.js
@@ -38,27 +38,33 @@ requirejs(['../src/WorldWind',
             wwd.addLayer(layers[l].layer);
         }
 
+        // Create the path's positions.
         var pathPositions = [];
         pathPositions.push(new WorldWind.Position(40, -100, 1e4));
         pathPositions.push(new WorldWind.Position(45, -110, 1e4));
         pathPositions.push(new WorldWind.Position(46, -122, 1e4));
+
+        // Create the path.
         var path = new WorldWind.Path(pathPositions);
         path.altitudeMode = WorldWind.RELATIVE_TO_GROUND;
         path.followTerrain = true;
-        path.extrude = true;
-        path.useSurfaceShapeFor2D = true;
+        path.extrude = true; // make it a curtain
+        path.useSurfaceShapeFor2D = true; // use a surface shape in 2D mode
 
+        // Create and assign the path's attributes.
         var pathAttributes = new WorldWind.ShapeAttributes(null);
         pathAttributes.outlineColor = WorldWind.Color.BLUE;
         pathAttributes.interiorColor = new WorldWind.Color(0, 1, 1, 0.5);
         pathAttributes.drawVerticals = path.extrude; // draw verticals only when extruding
         path.attributes = pathAttributes;
+
+        // Create and assign the path's highlight attributes.
         var highlightAttributes = new WorldWind.ShapeAttributes(pathAttributes);
         highlightAttributes.outlineColor = WorldWind.Color.RED;
         highlightAttributes.interiorColor = new WorldWind.Color(1, 1, 1, 0.5);
         path.highlightAttributes = highlightAttributes;
 
-        // Add the surface image to a layer and the layer to the World Window's layer list.
+        // Add the path to a layer and the layer to the World Window's layer list.
         var pathsLayer = new WorldWind.RenderableLayer();
         pathsLayer.displayName = "Paths";
         pathsLayer.addRenderable(path);
