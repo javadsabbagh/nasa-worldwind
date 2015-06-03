ecec8cd21eeaef3ea3f48228c65784a64988e26f
tgaskins <tgaskins@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2015-06-03 22:29:07 +0000

    Added the GoToLocation example.

---
+++ WebWorldWind/examples/GoToLocation.js
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2014 United States Government as represented by the Administrator of the
+ * National Aeronautics and Space Administration. All Rights Reserved.
+ */
+/**
+ * Illustrates how to use a GoToAnimator to smoothly move the view to a new location. This example listens for
+ * terrain picks and moves the view to the location picked.
+ *
+ * @version $Id$
+ */
+
+requirejs(['../src/WorldWind',
+        './LayerManager',
+        './CoordinateController'],
+    function (ww,
+              LayerManager,
+              CoordinateController) {
+        "use strict";
+
+        // Tell World Wind to log only warnings.
+        WorldWind.Logger.setLoggingLevel(WorldWind.Logger.LEVEL_WARNING);
+
+        // Create the World Window.
+        var wwd = new WorldWind.WorldWindow("canvasOne");
+
+        /**
+         * Added imagery layers.
+         */
+        var layers = [
+            {layer: new WorldWind.BMNGLayer(), enabled: true},
+            {layer: new WorldWind.BingAerialWithLabelsLayer(null), enabled: true},
+            {layer: new WorldWind.OpenStreetMapImageLayer(null), enabled: false},
+            {layer: new WorldWind.CompassLayer(), enabled: true},
+            {layer: new WorldWind.ViewControlsLayer(wwd), enabled: true}
+        ];
+
+        for (var l = 0; l < layers.length; l++) {
+            layers[l].layer.enabled = layers[l].enabled;
+            wwd.addLayer(layers[l].layer);
+        }
+
+        // Draw the World Window for the first time.
+        wwd.redraw();
+
+        // Create a layer manager for controlling layer visibility.
+        var layerManger = new LayerManager(wwd);
+
+        // Create a coordinate controller to update the coordinate overlay elements.
+        var coordinateController = new CoordinateController(wwd);
+
+        // Create a GoToAnimator that we'll use to move smoothly to picked locations.
+        var goToAnimator = new WorldWind.GoToAnimator(wwd);
+
+        // Now set up to handle clicks and taps.
+
+        // The common event-handling function.
+        var handleClick = function (o) {
+            // Obtain the event location.
+            var x = o.clientX,
+                y = o.clientY;
+
+            // Perform the pick. Must first convert from window coordinates to canvas coordinates, which are
+            // relative to the upper left corner of the canvas rather than the upper left corner of the page.
+            var pickList = wwd.pick(wwd.canvasCoordinates(x, y));
+
+            // If only one thing is picked and it is the terrain, use a go-to animator to go to the picked location.
+            if (pickList.objects.length == 1 && pickList.objects[0].isTerrain) {
+                var position = pickList.objects[0].position;
+                goToAnimator.goTo(new WorldWind.Location(position.latitude, position.longitude));
+            }
+        };
+
+        // Listen for mouse clicks.
+        var clickRecognizer = new WorldWind.ClickRecognizer(wwd);
+        clickRecognizer.addGestureListener(handleClick);
+
+        // Listen for taps on mobile devices.
+        var tapRecognizer = new WorldWind.TapRecognizer(wwd);
+        tapRecognizer.addGestureListener(handleClick);
+    });
