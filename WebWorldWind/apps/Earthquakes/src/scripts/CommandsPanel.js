393941b703a987e2ad18fcdcf17124262c1e753f
mevers <mevers@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2015-06-23 21:22:40 +0000

    Modularized some of the code.

--- WebWorldWind/apps/Earthquakes/src/scripts/CommandsPanel.js
+++ WebWorldWind/apps/Earthquakes/src/scripts/CommandsPanel.js
@@ -17,169 +17,69 @@ define(['Tour', 'TourManager'], function (Tour,
      * @param {WorldWindow} worldWindow The World Window to associate this layers panel with.
      */
     var CommandsPanel = function (wwd,layer) {
-        //this is all connected to the earthquake layer. You will need to entirely rewrite this section for different functionality.
-
-        //list of buttons id,name
-        this.buttons = [
-            ['CPanelUp','Greater Magnitude'],
-            ['CPanelDown','Lesser Magnitude'],
-            ['CPanelRight','Newer Earthquake'],
-            ["CPanelLeft",'Older Earthquake'],
-            ['StartTour','Start Tour'],
-            ['NewestEarth','Newest Earthquake']
-        ];
-
-        this.layer = layer;
         var self = this;
-        //the earthquake that we are looking at. The index of its location in the layer's parsed earthquake array.
-        this.FocusedEarthquake = 0;
+        this.layer = layer;
+        this.buttons = [];
+        this.bfunctions = [];
+        this.createDropDown('Panels');
+        //this.synchronizeLayerList();
+    };
 
-        //function that sorts an array and tracks the indexes change
-        function sortWithIndeces(toSortI,property) {
-            var toSort = toSortI.slice();
-            for (var i = 0; i < toSort.length; i++) {
-                toSort[i] = [toSort[i], i];
-            }
-            if (property){toSort.sort(function (left, right) {
-                return left[0][property] < right[0][property] ? -1 : 1;
-            });
-            } else {
-                toSort.sort(function (left, right) {
-                    return left[0] < right[0] ? -1 : 1;
-                });
-            }
-
-            toSort.sortIndices = [];
-            for (var j = 0; j < toSort.length; j++) {
-                toSort.sortIndices.push(toSort[j][1]);
-                toSort[j] = toSort[j][0];
-            }
-            return toSort;
-        }
-
-        //create a 1-1 map from the indexes of the array sorted by time to the array sorted by magnitude
-        var indexis = sortWithIndeces(layer.Manage.ParsedData,'magnitude').sortIndices;
-        var OrIndexMtD = function (ind) {
-            return indexis[ind];
-        };
-        //the inverse function
-        var indexis2 = sortWithIndeces(indexis).sortIndices;
-        var OrIndexDtM = function (ind) {
-            return indexis2[ind];
-        };
-        //recalculate the mapping
-        this.resetData = function() {
-            this.FocusedEarthquake = 0;
-            indexis = sortWithIndeces(layer.Manage.ParsedData,'magnitude').sortIndices;
-            indexis2 = sortWithIndeces(indexis).sortIndices;
-        };
-        //used to animate the movements
-        var goToAnimator = new WorldWind.GoToAnimator(wwd);
-        //repeated code for buttons
-        var ShowChanges = function (INDEX) {
-            var layer = self.layer;
+    CommandsPanel.prototype.initMouseClickListener = function () {
+        var self = this;
+        var ShowChanges = function (layerArg,INDEX) {
+            var layer = layerArg;
             var display = $('#eData');
             display.empty();
-            display.append('<p>' + layer.Manage.ParsedData[INDEX].info + '</p>');
-            layer.Manage.Animations.animate(layer.renderables[INDEX])
-            var positionAlias = Object.create(layer.Manage.ParsedData[INDEX].position);
-            positionAlias.altitude = 2000000;
-            goToAnimator.cancel();
-            goToAnimator.goTo(positionAlias);
-
+            display.append('<p>' + layerArg.Manage.ParsedData[INDEX].info + '</p>');
+            layerArg.Manage.Animations.animate(layerArg.renderables[INDEX]);
         };
-
-
-        var quakesInTour = layer.Manage.ParsedData.filter(function(quake) {
-            return quake.magnitude >= 5;
-        });
-
-        function getQuakePosition(quake) {
-            return new WorldWind.Position(quake.lat, quake.long, 100000);
-        }
-
-        var magnitudeTour = new Tour('Magnitude Tour',quakesInTour, getQuakePosition, function(q1, q2) {
-            return Math.ceil(q1.magnitude - q2.magnitude);
-        });
-        var magnitudeTourManager = new TourManager(magnitudeTour, goToAnimator);
-
-        var timeTour = new Tour('Time tour', quakesInTour, getQuakePosition, function(q1, q2) {
-            var t1 = q1.time;
-            var t2 = q2.time;
-            if(t1 < t2) {
-                return -1;
-            } else if (t1 > t2) {
-                return 1;
-            }
-            return 0;
-        });
-        var timeTourManager = new TourManager(timeTour, goToAnimator);
-
-        //this variable contains the functions that will be applied in the same order as the buttons are in the array.
-        this.bfunctions = [
-            function (event) {
-                if (OrIndexDtM(self.FocusedEarthquake)+1 < self.layer.Manage.ParsedData.length){
-                    self.FocusedEarthquake = OrIndexMtD(OrIndexDtM(self.FocusedEarthquake) + 1);
-                }
-                ShowChanges(self.FocusedEarthquake);
-            },
-            function (event) {
-                if (OrIndexDtM(self.FocusedEarthquake) > 0){
-                    self.FocusedEarthquake = OrIndexMtD(OrIndexDtM(self.FocusedEarthquake) - 1);
-                }
-                ShowChanges(self.FocusedEarthquake);
-            },
-            function (event) {
-                if (self.FocusedEarthquake+1 < self.layer.Manage.ParsedData.length){
-                    self.FocusedEarthquake = self.FocusedEarthquake + 1;
-                }
-                ShowChanges(self.FocusedEarthquake);
-            },
-            function (event) {
-                if (self.FocusedEarthquake - 1 > 0) {
-                    self.FocusedEarthquake = self.FocusedEarthquake - 1;
-                }
-                ShowChanges(self.FocusedEarthquake);
-            },
-            function (event) {
-                if (!magnitudeTourManager.tourRun) {
-                    magnitudeTourManager.startTour();
-                    console.log('running');
-                    $(event.target).attr('class','btn btn-danger');
-                    $(event.target).text("Stop Touring");
-                    magnitudeTourManager.addCallback(function(t){
-                        $(event.target).attr('class','btn btn-primary');
-                        $(event.target).text("Start Tour");
-                    });
-                }else{
-                    magnitudeTourManager.stopTour();
-                    console.log('stopped');
-                    $(event.target).attr('class','btn btn-primary');
-                    $(event.target).text("Start Tour");
-                }
-
-            },
-            function(event){
-                ShowChanges(self.FocusedEarthquake = 0);
-            }
-        ];
-
-
         //whenever you click on an earthquake, it zooms in on that.
         document.getElementById("canvasOne").onmousedown = function tss (e) {
             console.log(e)
             if (e.which === 1 && e.type === 'mousedown'){
-                for (var i in layer.renderables) {
-                    if (layer.renderables[i].highlighted) {
-                        console.log('hi')
-                        ShowChanges(i)
+                for (var i in self.layer.renderables) {
+                    if (self.layer.renderables[i].highlighted) {
+                        ShowChanges(self.layer,i)
                     }
 
                 }
             }
         };
+    };
 
-        this.synchronizeLayerList();
+    CommandsPanel.prototype.createDropDown = function (htmlLoc) {
+        var panel = $('<button>');
+        panel.attr("type",'button')
+        panel.attr("class","btn btn-info btn-block dropdown-toggle");
+        panel.attr("data-toggle","collapse");
+        panel.attr("data-target","#cp");
+        panel.text(this.layer.displayName + " Panel");
+        panel.append('<span class="' + 'caret' + '"></span>')
+        var divi1 = $("<div>");
+        divi1.attr("id","cp");
+        divi1.attr("class","collapse");
+        var divi2 = $("<div>");
+        divi2.attr('class','list-group');
+        divi2.attr('id','CPanel');
+        divi1.append(divi2);
+        $('#' + htmlLoc).append(panel);
+        $('#' + htmlLoc).append(divi1);
+    };
+
+    CommandsPanel.prototype.addButton = function(buttonIDName) {
+        //looks like ['id','name']
+        this.buttons.push(buttonIDName);
+    };
+
+    CommandsPanel.prototype.addFunctionToButton = function (buttonID,funct) {
+        for (var i = 0; i < this.buttons.length; i++){
+            if (this.buttons[i][0]===buttonID){
+                this.bfunctions[i] = (funct);
+            };
+
+        };
+
     };
 
     CommandsPanel.prototype.onLayerClick = function (layerButton) {
@@ -193,6 +93,8 @@ define(['Tour', 'TourManager'], function (Tour,
         layerListItem.find("button").off("click");
         layerListItem.find("button").remove();
         */
+
+
         // Synchronize the displayed layer list with the World Window's layer list.
         for (var i = 0; i < this.buttons.length; i++) {
             var index = i
@@ -204,13 +106,20 @@ define(['Tour', 'TourManager'], function (Tour,
             var d2 = $('<button>');
             d2.attr("class","btn btn-primary");
             d2.attr("id","Button " + self.layer.displayName + ' ' + self.buttons[i][1]);
-            d2.text(this.buttons[i][1]);
-            d2.on("click",
-                self.bfunctions[index]
-            );
+            d2.text(self.buttons[index][1]);
+            console.log(self.bfunctions[index])
+            if (self.bfunctions[index] !== undefined){
+                d2.on("click",
+                    //for whatever reason, you can't see var i from the for loop in here...
+                    self.bfunctions[index]
+
+                );
+
+            }
             d1.append(d2);
             d.append(d1);
             layerListItem.append(d);
+            //add a red separator
             if (i === this.buttons.length-1){
                 var d = $("<div>");
                 d.attr("class","btn-group btn-group-justified");
--- WebWorldWind/apps/Earthquakes/src/scripts/EarthquakeViewer.js
+++ WebWorldWind/apps/Earthquakes/src/scripts/EarthquakeViewer.js
@@ -37,14 +37,14 @@ define(['http://worldwindserver.net/webworldwind/worldwindlib.js',
             {layer: new WorldWind.BingAerialWithLabelsLayer(null), enabled: false},
             {layer: new WorldWind.OpenStreetMapImageLayer(null), enabled: false},
             {layer: new WorldWind.CompassLayer(), enabled: true},
-            {layer: new WorldWind.ViewControlsLayer(wwd), enabled: true},
-
+            {layer: new WorldWind.ViewControlsLayer(wwd), enabled: true}
         ];
 
         for (var l = 0; l < layers.length; l++) {
             layers[l].layer.enabled = layers[l].enabled;
             wwd.addLayer(layers[l].layer);
         }
+        var arrowLay = new WorldWind.RenderableLayer()
 
         //displays info of highlighted earthquake in eData division, also sets the significant earthquake when clicked
         var displayInfo = function (layer) {
@@ -113,6 +113,9 @@ define(['http://worldwindserver.net/webworldwind/worldwindlib.js',
 
                     //again animates the most recent displayed earthquake. When the renderables change, the renderable ceases to be animated.
                     newLayer.Manage.Animations.animate(newLayer.renderables[0]);
+
+                    //log the changes to the list of earthquakes
+                    commandsPanel.resetData();
                 });
 
                 var goToAnimator = new WorldWind.GoToAnimator(wwd);
@@ -126,8 +129,6 @@ define(['http://worldwindserver.net/webworldwind/worldwindlib.js',
                         magSlider.slider('setValue',mag);
                     }
                 }
-
-
 
                 // Add mechanism for handling uri query parameters
                 function goToCallback(qs) {
@@ -202,6 +203,45 @@ define(['http://worldwindserver.net/webworldwind/worldwindlib.js',
                 console.log('quakes ', arg);
 
 
+                //this is all commands panel stuff
+
+                //function that sorts an array and tracks the indexes change
+                function sortWithIndeces(toSortI,property) {
+                    var toSort = toSortI.slice();
+                    for (var i = 0; i < toSort.length; i++) {
+                        toSort[i] = [toSort[i], i];
+                    }
+                    if (property){toSort.sort(function (left, right) {
+                        return left[0][property] < right[0][property] ? -1 : 1;
+                    });
+                    } else {
+                        toSort.sort(function (left, right) {
+                            return left[0] < right[0] ? -1 : 1;
+                        });
+                    }
+
+                    toSort.sortIndices = [];
+                    for (var j = 0; j < toSort.length; j++) {
+                        toSort.sortIndices.push(toSort[j][1]);
+                        toSort[j] = toSort[j][0];
+                    }
+                    return toSort;
+                }
+
+                //for commands panel layer.
+                var ShowChanges = function (layerArg,INDEX) {
+                    var layer = layerArg;
+                    var display = $('#eData');
+                    display.empty();
+                    display.append('<p>' + layerArg.Manage.ParsedData[INDEX].info + '</p>');
+                    layerArg.Manage.Animations.animate(layerArg.renderables[INDEX])
+                    var positionAlias = Object.create(layerArg.Manage.ParsedData[INDEX].position);
+                    positionAlias.altitude = 2000000;
+                    goToAnimator.cancel();
+                    goToAnimator.goTo(positionAlias);
+
+                };
+
                 var quakesInTour = arg.filter(function(quake) {
                    return quake.magnitude >= 5;
                 });
@@ -227,16 +267,99 @@ define(['http://worldwindserver.net/webworldwind/worldwindlib.js',
                 });
                 var timeTourManager = new TourManager(timeTour, goToAnimator);
 
-
                 var commandsPanel = new CommandsPanel(wwd,newLayer);
 
+                //create a 1-1 map from the indexes of the array sorted by time to the array sorted by magnitude
+                var indexis = sortWithIndeces(commandsPanel.layer.Manage.ParsedData,'magnitude').sortIndices;
+                var OrIndexMtD = function (ind) {
+                    return indexis[ind];
+                };
 
+                //the inverse function
+                var indexis2 = sortWithIndeces(indexis).sortIndices;
+                var OrIndexDtM = function (ind) {
+                    return indexis2[ind];
+                };
+
+                //recalculate the mapping
+                commandsPanel.resetData = function() {
+                    commandsPanel.FocusedEarthquake = 0;
+                    indexis = sortWithIndeces(commandsPanel.layer.Manage.ParsedData,'magnitude').sortIndices;
+                    indexis2 = sortWithIndeces(indexis).sortIndices;
+                };
+                //repeated code for buttons
+                //this variable contains the functions that will be applied in the same order as the buttons are in the array.
+                var self = commandsPanel;
+                //list of buttons id,name
+                var buttonsForCPanel = [
+                    ['CPanelUp','Greater Magnitude'],
+                    ['CPanelDown','Lesser Magnitude'],
+                    ['CPanelRight','Newer Earthquake'],
+                    ["CPanelLeft",'Older Earthquake'],
+                    ['StartTour','Start Tour'],
+                    ['NewestEarth','Newest Earthquake']
+                ];
+                for (var i = 0; i<buttonsForCPanel.length; i++){
+                    commandsPanel.addButton(buttonsForCPanel[i])
+                };
+                var buttonFunctionsForCPanel = [
+                    function (event) {
+                        if (OrIndexDtM(self.FocusedEarthquake)+1 < self.layer.Manage.ParsedData.length){
+                            self.FocusedEarthquake = OrIndexMtD(OrIndexDtM(self.FocusedEarthquake) + 1);
+                        }
+                        ShowChanges(commandsPanel.layer,self.FocusedEarthquake);
+                    },
+                    function (event) {
+                        if (OrIndexDtM(self.FocusedEarthquake) > 0){
+                            self.FocusedEarthquake = OrIndexMtD(OrIndexDtM(self.FocusedEarthquake) - 1);
+                        }
+                        ShowChanges(commandsPanel.layer,self.FocusedEarthquake);
+                    },
+                    function (event) {
+                        if (self.FocusedEarthquake+1 < self.layer.Manage.ParsedData.length){
+                            self.FocusedEarthquake = self.FocusedEarthquake + 1;
+                        }
+                        ShowChanges(commandsPanel.layer,self.FocusedEarthquake);
+                    },
+                    function (event) {
+                        if (self.FocusedEarthquake - 1 > 0) {
+                            self.FocusedEarthquake = self.FocusedEarthquake - 1;
+                        }
+                        ShowChanges(commandsPanel.layer,self.FocusedEarthquake);
+                    },
+                    function (event) {
+                        if (!magnitudeTourManager.tourRun) {
+                            magnitudeTourManager.startTour();
+                            console.log('running');
+                            $(event.target).attr('class','btn btn-danger');
+                            $(event.target).text("Stop Touring");
+                            magnitudeTourManager.addCallback(function(t){
+                                $(event.target).attr('class','btn btn-primary');
+                                $(event.target).text("Start Tour");
+                            });
+                        }else{
+                            magnitudeTourManager.stopTour();
+                            console.log('stopped');
+                            $(event.target).attr('class','btn btn-primary');
+                            $(event.target).text("Start Tour");
+                        }
+
+                    },
+                    function(event){
+                        ShowChanges(commandsPanel.layer,self.FocusedEarthquake = 0);
+                    }
+                ];
+                for (var i = 0; i<buttonFunctionsForCPanel.length; i++){
+                    commandsPanel.addFunctionToButton(buttonsForCPanel[i][0],buttonFunctionsForCPanel[i]);
+                };
+
+                commandsPanel.FocusedEarthquake = 0;
+                commandsPanel.synchronizeLayerList();
+                commandsPanel.initMouseClickListener();
 
                 // Uncomment to initiate tours
                 //magnitudeTourManager.startTour();
                 //timeTourManager.startTour();
-
-
 
             });
 
