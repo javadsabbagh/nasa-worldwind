909dcf06c51b281250bc39d4ae7178501d55bd5d
tgaskins <tgaskins@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2015-06-24 19:49:10 +0000

    Added GeographicMesh shape and example.

---
+++ trunk/WebWorldWind/examples/GeographicMeshes.js
@@ -0,0 +1,89 @@
+/*
+ * Copyright (C) 2014 United States Government as represented by the Administrator of the
+ * National Aeronautics and Space Administration. All Rights Reserved.
+ */
+/**
+ * Illustrates how to display GeographicMesh shapes.
+ *
+ * @version $Id$
+ */
+
+requirejs(['../src/WorldWind',
+        './LayerManager',
+        './CoordinateController'],
+    function (ww,
+              LayerManager,
+              CoordinateController) {
+        "use strict";
+
+        // Tell World Wind to log only warnings and errors.
+        WorldWind.Logger.setLoggingLevel(WorldWind.Logger.LEVEL_WARNING);
+
+        // Create the World Window.
+        var wwd = new WorldWind.WorldWindow("canvasOne");
+
+        /**
+         * Add imagery layers.
+         */
+        var layers = [
+            {layer: new WorldWind.BMNGLayer(), enabled: true},
+            {layer: new WorldWind.BingAerialWithLabelsLayer(null), enabled: true},
+            {layer: new WorldWind.OpenStreetMapImageLayer(null), enabled: false},
+            {layer: new WorldWind.CompassLayer(), enabled: true},
+            {layer: new WorldWind.ViewControlsLayer(wwd), enabled: true}
+        ];
+
+        for (var l = 0; l < layers.length; l++) {
+            layers[l].layer.enabled = layers[l].enabled;
+            wwd.addLayer(layers[l].layer);
+        }
+
+        // Create the mesh's positions.
+        var meshPositions = [];
+        for (var lat = 30; lat <= 35; lat += 0.5) {
+            var row = [];
+            for (var lon = -120; lon <= -110; lon += 0.5) {
+                // Create elevations that follow a sine wave in latitude and a cosine wave in longitude.
+                var elevationScale =
+                    Math.sin(((lat - 30) / 5) * 2 * Math.PI) * Math.cos(((lon + 120) / 10) * 2 * Math.PI);
+
+                row.push(new WorldWind.Position(lat, lon, 100e3 * (1 + elevationScale)));
+            }
+
+            meshPositions.push(row);
+        }
+
+        // Create the mesh.
+        var mesh = new WorldWind.GeographicMesh(meshPositions);
+
+        // Create and assign the mesh's attributes.
+        var meshAttributes = new WorldWind.ShapeAttributes(null);
+        meshAttributes.outlineColor = WorldWind.Color.BLUE;
+        meshAttributes.interiorColor = new WorldWind.Color(0, 1, 1, 0.7);
+        meshAttributes.imageSource = "../images/400x230-splash-nww.png";
+        mesh.attributes = meshAttributes;
+
+        // Create and assign the mesh's highlight attributes.
+        var highlightAttributes = new WorldWind.ShapeAttributes(meshAttributes);
+        highlightAttributes.outlineColor = WorldWind.Color.RED;
+        highlightAttributes.interiorColor = new WorldWind.Color(1, 1, 1, 0.7);
+        mesh.highlightAttributes = highlightAttributes;
+
+        // Add the mesh to a layer and the layer to the World Window's layer list.
+        var meshLayer = new WorldWind.RenderableLayer();
+        meshLayer.displayName = "Geographic Mesh";
+        meshLayer.addRenderable(mesh);
+        wwd.addLayer(meshLayer);
+
+        // Draw the World Window for the first time.
+        wwd.redraw();
+
+        // Create a layer manager for controlling layer visibility.
+        var layerManger = new LayerManager(wwd);
+
+        // Create a coordinate controller to update the coordinate overlay elements.
+        var coordinateController = new CoordinateController(wwd);
+
+        // Now set up to handle highlighting.
+        var highlightController = new WorldWind.HighlightController(wwd);
+    });
