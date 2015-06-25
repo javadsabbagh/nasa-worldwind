33d9f03e1703c690aec856c28fe972d6cd7d9500
irahaman <irahaman@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2015-06-25 19:43:18 +0000

    Added path to requirejs condiguration; started to integrate Set with layer

--- trunk/WebWorldWind/apps/OpenStreetMap/js/main.js
+++ trunk/WebWorldWind/apps/OpenStreetMap/js/main.js
@@ -2,6 +2,7 @@
 
 requirejs.config({
     paths: {
+        "worldwind" : "http://worldwindserver.net/webworldwind/worldwindlib.js",
         "jquery" : "//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min",
         "OpenStreetMapApp" : "OpenStreetMapApp"
     }
