eae8209ab77a42447f701fe355fd30509152f7bc
mevers <mevers@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2015-06-25 18:53:47 +0000

    Now parses JSON data to a more usable structure.

--- trunk/WebWorldWind/apps/OpenStreetMap/js/OSMDataRetriever.js
+++ trunk/WebWorldWind/apps/OpenStreetMap/js/OSMDataRetriever.js
@@ -5,28 +5,89 @@ define(['xmlToJSON'],
 
         };
 
+        /*
+         Builds the API call and returns it.
+
+         @param boundingBoxCoords: Contains an array of points that determine the bounding box.
+                                   [LEFT , BOTTOM , RIGHT , TOP]
+         @return: API call URL
+         */
         OSMDataRetriever.prototype.buildAPICall = function(boundingBoxCoords){
             //console.log('Retrieving data location...');
             var param = boundingBoxCoords.join(',');
             return 'http://api06.dev.openstreetmap.org/api/0.6/map?bbox=' + param;
         };
 
+        /*
+        Uses an external library to change the xml data structure to JSON data structure
+
+        @param data: XML OSM data structure.
+        @return: JSON data structure containing the same information as the xml data structure.
+         */
         OSMDataRetriever.prototype.translateOSMData = function (data) {
             console.log('Translating OSM XML data to JSON...');
             return XTJ(data)
         };
 
+        /*
+        Parses JSON data into a more understandable and useable form.
+
+        @param JSOND: JSON data structure in the form outputted from translateOSMData().
+         */
+        OSMDataRetriever.prototype.reduceOSMData = function (JSOND) {
+            console.log('Interpreting JSON Data...')
+            var reducedJSON = {
+                _overHead: {
+                    boundingBox: [
+                        JSOND.osm['bounds']['@attributes']['minlon'],
+                        JSOND.osm['bounds']['@attributes']['minlat'],
+                        JSOND.osm['bounds']['@attributes']['maxlon'],
+                        JSOND.osm['bounds']['@attributes']['maxlat']
+                    ]
+                },
+                nodes: []
+            };
+            for (var oInfo in JSOND.osm['@attributes']){
+                reducedJSON._overHead[oInfo] = JSOND.osm['@attributes'][oInfo]
+            }
+            var index = 0;
+            JSOND.osm.node.forEach(function(node){
+                reducedJSON.nodes[index] = {
+                    _overHead: {
+                        timestamp: node['@attributes'].timestamp
+                    },
+                    location: {
+                        lat: node['@attributes'].lat,
+                        lon: node['@attributes'].lon
+                    },
+                    info: {}
+                };
+                node.tag.forEach(function(tag){
+                    reducedJSON.nodes[index].info[tag['@attributes'].k] = tag['@attributes'].v
+                });
+                index++;
+            });
+            return reducedJSON;
+        };
+
+        /*
+        Requests OSM data and executes a callback function when data is retrieved.
+
+        @param boundingBoxCoords: Contains an array of points that determine the bounding box.
+                                  [LEFT , BOTTOM , RIGHT , TOP]
+        @param callback: Function to be executed when data is retrieved.
+         */
         OSMDataRetriever.prototype.requestOSMData = function(boundingBoxCoords, callback){
             console.log('Fetching OSM Data...');
-            var self = this
+            var self = this;
             var url = this.buildAPICall(boundingBoxCoords);
             $.get(url, function(data) {
-                console.log('OSM Data Fetched!')
-                console.log(data);
-                var JSONData = self.translateOSMData(data);
-                console.log('Data Converted to JSON!')
-                console.log(JSONData);
-                callback(JSONData);
+                console.log('OSM Data Fetched!');
+                console.log(self.translateOSMData(data));
+                var rData = self.reduceOSMData(self.translateOSMData(data));
+                console.log('Data Converted to JSON!');
+                console.log(rData);
+                callback(rData);
             });
         };
 
