10fa2e72929810b69a32c7aaf570c48c45b10377
mevers <mevers@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2015-06-29 16:49:43 +0000

    Documented order in which bounding box should be written.

--- trunk/WebWorldWind/apps/OpenStreetMap/js/OverpassAPIWrapper.js
+++ trunk/WebWorldWind/apps/OpenStreetMap/js/OverpassAPIWrapper.js
@@ -12,7 +12,7 @@ define(['jquery','OpenStreetMapConfig', 'osmtogeojson'],function($, OpenStreetMa
         Assembles a query for the Overpass API using a boundingBox and a list of amenities
         to consider. If the amenities list is undefined, null, or false, then a query is
         constructed to get all nodes with an amenity tag.
-        @param boundingBox: the bounding box to consider
+        @param boundingBox: the bounding box to consider [lower latitude, lower longitude, upper latitude, upper longitude]
         @param amenities: the amenities to consider
         @return a Overpass query to to access nodes with amenities in a specified bounding box
      */
@@ -31,7 +31,7 @@ define(['jquery','OpenStreetMapConfig', 'osmtogeojson'],function($, OpenStreetMa
     /*
         Assembles an API to retrieve the nodes with specified amenities in a specified bounding box
         (NB: if amenities is null, false, or undefined, then we retrieve all nodes in the bounding box)
-        @param boundingBox : the bounding box to consider
+        @param boundingBox : the bounding box to consider [lower latitude, lower longitude, upper latitude, upper longitude]
         @param amenities: the amenities to consider
         @return : a url that can be used to retrieve amenities in a specified bounding box using
                   the Overpass Open Street Map API
@@ -44,7 +44,7 @@ define(['jquery','OpenStreetMapConfig', 'osmtogeojson'],function($, OpenStreetMa
     /*
         Given a bounxing box and a callback to handle the data, uses a GET request to
         access Open Street Map data that contains amenities.
-        @param boundingBox: the bounding box to consider
+        @param boundingBox: the bounding box to consider [lower latitude, lower longitude, upper latitude, upper longitude]
         @param callback : the callback to use to handle the data retrived from the GET request
      */
     OverpassAPIWrapper.prototype.getAllAmenitiesInBox = function(boundingBox, callback) {
