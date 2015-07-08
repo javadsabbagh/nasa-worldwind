eb2eced7a6bae028e229544b04a958ac5dcdda39
mevers <mevers@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2015-07-08 23:54:51 +0000

    Commented Last Commit

--- WebWorldWind/apps/OpenStreetMap/js/NaturalLanguageHandler.js
+++ WebWorldWind/apps/OpenStreetMap/js/NaturalLanguageHandler.js
@@ -10,8 +10,9 @@ define(['OverpassAPIWrapper','RouteAPIWrapper','OSMDataRetriever'],
         function NaturalLanguageHandler (wwd){
             var self = this;
             this._wwd = wwd;
-            self.nearNames = ['Elgin','Near Me'];
-            self.places = ['Starbucks'];
+            // THIS IS ALL FOR MATT. NOTES ON HOW I WANTED TO DO THE SEARCH.
+            // self.nearNames = ['Elgin','Near Me'];
+            // self.places = ['Starbucks'];
             //I am at _here/name__ near _location__ looking for __key/name__ within __miles__ of _location__
             //OR I am looking for __key/name__ within __miles__ of _location__
             //build bounding box based on location and miles
@@ -20,75 +21,135 @@ define(['OverpassAPIWrapper','RouteAPIWrapper','OSMDataRetriever'],
             //if more than one turns up, clarify
             //get each location
             //route it
-
         };
+
+        /* Call receiveInput with the output of the natural language search as a parameter.
+        * 1 - translateAndSend(RawInputFromNaturalLanguageSearch, CallbackS1) -- Change 'near me' string to user's location.
+        * 2 - callbackS1(translatedInput) -- Change BB center to Bounding Box array... I don't know why I didn't just
+        *                                       put this in translateAndSend...
+        * 3 - clarityCallback(RawInputFromOverpassAPI) -- return data containing exactly one feature to the routing funcs
+        *
+        * @param someInput: Array containing the words corrosponding to the selection in the
+        *                   natural language search sentence. Looks like
+        *                   [Location of Center of BB, Key search for API, value of Key for API, ...]
+        **/
 
         NaturalLanguageHandler.prototype.receiveInput = function (someInput, callback) {
             var self = this;
             var OSMDR;
-            // var someBB = [42.00,-88.35,42.15,-88.2];
-            // Each entry in someInput corrosponds to a blank in the NLS
-            someInput = ['Starbucks','Near Me'];
 
-            // This checks if there is more than one returned location and then asks the user to clarify.
+            // Default NHS input...
+            if (!someInput){
+                someInput = ['Near Me','name','Walmart'];
+            }
+
+            /* This 'modifies' the callback so that if the API returns more than one feature, it asks the user
+            *  to choose one. This is called once all the input strings are turned into usable numbers.
+            *
+            *  @param data: This is the data that is returned from calling Overpass API
+            **/
+
             var clarityCallback = function (data) {
                 if (data.features.length > 1) {
-                    // Put something here to handle the NLH
-                    // Append or initiate a second natural language handler
-                    console.log('Too Many features.')
-                    callback(data)
+
+                    // FUNCTION HERE: Add a function that tells the canvas to prompt the user for clarification.
+                    //                prompt is used A.T.M.
+                    // WhichLocation asks the user to choose the feature to select for directions.
+                    var whichLocation = prompt("Type the number of the location of...(Temp) " + (data.features.length-1).toString(), 0);
+
+                    // If the user chose a number, return data.features equal to an array containing that element.
+                    if (whichLocation != null){
+                        data.features = [data.features[whichLocation]];
+                        callback(data)
+                    }
+
+                    // FUNCTION HERE: Add a function that tells the canvas to hide.
                 } else {
-                    console.log('One or No features.')
-                    callback(data)
-                    // something here to slide the overlay over
+                    callback(data);
+
+                    // FUNCTION HERE: Add a function that tells the canvas to hide.
                 }
             };
 
-            // Step 1, translate someInput
-            var TranslateAndSend = function (input, callbackS1) {
-                var defaultLocation = [42.075, -88.275];
-                // Do nothing with the name right now.
-                // input[0] = input[0]
-                var infoToPass = [];
-                infoToPass[0] = input[0]
+            /* This takes the strings from the input and turns them into arrays/numbers.
+            *
+            *   @param input: input should be the array of strings corrosponding to selected words in the NHL.
+            *   @param callbackS1: this should be the call back function that turns the bounding box center into
+            *                       a bounding box array. That callback should then call the API.
+            **/
 
-                // Below here is for finding the BB center. This does not build the BB yet.
-                if (input[1] === 'Near Me'){
+            var TranslateAndSend = function (input, callbackS1) {
+
+                // This is matts house. This is in case the navigator fails to retrieve user's location.
+                var defaultLocation = [42.038, -88.323];
+
+                // infoToPass[0] is the array [lat, long] of the start location.
+                // infoToPass[1] is the key for the Overpass API
+                // infoToPass[2] is the value of the key for the Overpass API, this may be blank to return all values
+                //              of the key in the bounding box.
+                var infoToPass = [];
+                infoToPass[1] = input[1];
+                infoToPass[2] = input[2];
+
+                // If the user inputs 'Near Me' as the start location, it tries to use the users current position.
+                if (input[0] === 'Near Me'){
                     if (navigator.geolocation) {
                         var returnUsersLocation = function(inputFromGCP){
-                            infoToPass[1] = inputFromGCP.coords;
+                            infoToPass[0] = inputFromGCP.coords;
                             callbackS1(infoToPass);
                         };
                         var returnDefaultLocation = function(inputFromGCP){
-                            // This is by my house
                             console.log('Error finding users location.')
-                            infoToPass[1] = defaultLocation;
+                            infoToPass[0] = defaultLocation;
                             callbackS1(infoToPass);
                         };
+
+                        // Sets the location to get directions from as the user's location.
+                        // If it fails to get the user's location, it goes to the default location... Matt's house.
                         navigator.geolocation.getCurrentPosition(returnUsersLocation, returnDefaultLocation);
                     }
                 } else {
-                    //look up what the location of this is.
-                    infoToPass[1] = defaultLocation;
+
+                    //Currently this is here so that it returns the default location if nothing else.
+                    // FUNCTION HERE: Put a function here to determine the 'location' of the string and set it
+                    //                  to infoToPass[0]
+                    infoToPass[0] = defaultLocation;
                     callbackS1(infoToPass)
                 }
             };
 
-            // Use this for the step1 callback.
+            /* Builds the bounding box based around a center.
+            *
+            *   @param translatedArrayData: The input array from the NHL after it has been
+            *                               processed by translateAndSend.
+            **/
+
             var areaFetcherWrapper = function(translatedArrayData){
+
+                // Create an open street map API caller if not done already.
                 if (!OSMDR){OSMDR = new ODR()}
-                var boundingBox = self.buildBB(translatedArrayData[1])
-                OSMDR.requestOSMData(boundingBox, 'name', clarityCallback, translatedArrayData[0])
+
+                // Build the bounding box around the center.
+                var boundingBox = self.buildBB(translatedArrayData[0]);
+
+                // Call the API with the translated data and bounding box and return the data from the API
+                //      to the callback.
+                OSMDR.requestOSMData(boundingBox, translatedArrayData[1], clarityCallback, translatedArrayData[2])
             }
 
+            // Calls the API after someInput is translated and the bounding box is determined.
+            // Translate(someInput) -> areaFetcherWrapper(translatedSomeInput)
             TranslateAndSend(someInput, areaFetcherWrapper)
 
-            // areaFetcher.requestOSMData(someBB, 'name', clarityCallback, someInput[0])
         };
+
+        /* Builds the bounding box based around a center.
+        *
+        *   @param BBCenter: The center of where the bounding box needs to be.
+        **/
 
         NaturalLanguageHandler.prototype.buildBB = function (BBCenter){
             return [BBCenter[0] -.075, BBCenter[1] -.075, BBCenter[0] +.075, BBCenter[1] +.075]
-                //something here to build a bounding box based on location given
         };
 
 
