343f330304de20969809763e6b3da5baf0f08aaf
irahaman <irahaman@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2015-07-14 22:50:16 +0000

    Refacroted code, added geocoding, added more facilities for natural language search, modularized code

--- trunk/WebWorldWind/apps/OpenStreetMap/js/OpenStreetMapConfig.js
+++ trunk/WebWorldWind/apps/OpenStreetMap/js/OpenStreetMapConfig.js
@@ -15,7 +15,7 @@ define(['http://worldwindserver.net/webworldwind/worldwindlib.js',
 
         this._drawHeightInKm = this._drawHeightInM * 1000;
 
-        this._drawRadiusInMiles = 10.0;
+        this._drawRadiusInMiles = 20.0;
 
         this._kmPerMile = 1.60934;
 
@@ -38,13 +38,39 @@ define(['http://worldwindserver.net/webworldwind/worldwindlib.js',
         this._overPassAPIBody =
             'http://overpass-api.de/api/interpreter?data=';
 
-        this._OSRMAPIBody = 'http://router.project-osrm.org/'
+        this._OSRMAPIBody = 'http://router.project-osrm.org/';
+
+        this._startPosition =  new WorldWind.Position(42.0362415,-88.3450904, 10000);
 
         this._temp = 'http://overpass-api.de/api/interpreter?data=node%5B%22amenity%22%7E%22%2E%22%5D%2852%2E5167%2C13%2E3833%2C53%2C14%29%3Bout%20body%3B%0A'
+
+        this._defaultInputSpecification = {
+            useCurrentLocationForNavigation : false,
+            overpassKey : 'amenity',
+            overpassValue : 'cafe'
+        };
+
+        this._boundingBoxWidth = 0.75;
+
+        this._boundingBoxHeight = 0.75;
+
+        this._nodeLimit = 100;
+
+        this._mapQuestAPIBase = 'http://www.mapquestapi.com/geocoding/v1/address?key=';
+        this._mapQuestAPIKey = 'oa1pl9hIxUNigL3uwrAkERWczhGqDdAk';
+
     }
 
 
     Object.defineProperties(OpenStreetMapConfig.prototype, {
+
+
+        startPosition: {
+            get: function() {
+                return this._startPosition;
+            }
+        },
+
         canvasName : {
             get: function() {
                 return this._canvasName;
@@ -119,6 +145,42 @@ define(['http://worldwindserver.net/webworldwind/worldwindlib.js',
         OSRMAPIBody : {
             get: function() {
                 return this._OSRMAPIBody;
+            }
+        },
+
+        defaultInputSpecification: {
+            get : function() {
+                return this._defaultInputSpecification;
+            }
+        },
+
+        boundingBoxWidth : {
+            get: function() {
+                return this._boundingBoxWidth;
+            }
+        },
+
+        boundingBoxHeight : {
+            get: function() {
+                return this._boundingBoxHeight;
+            }
+        },
+
+        nodeLimit : {
+            get: function() {
+                return this._nodeLimit;
+            }
+        },
+
+        mapQuestAPIBase : {
+            get: function() {
+                return this._mapQuestAPIBase;
+            }
+        },
+
+        mapQuestAPIKey : {
+            get: function() {
+                return this._mapQuestAPIKey;
             }
         }
     });
