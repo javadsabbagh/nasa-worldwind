8615a79178805205ec76f464e4f79a0005396320
mevers <mevers@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2015-07-22 01:45:31 +0000

    Removed extra comma

--- WebWorldWind/apps/NaturalLanguageForm/js/main.js
+++ WebWorldWind/apps/NaturalLanguageForm/js/main.js
@@ -4,7 +4,7 @@ requirejs.config({
     paths: {
         "worldwind" : "http://worldwindserver.net/webworldwind/worldwindlib.js",
         "jquery" : "http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min",
-        "OpenStreetMapApp" : "OpenStreetMapApp",
+        "OpenStreetMapApp" : "OpenStreetMapApp"
     }
 });
 
