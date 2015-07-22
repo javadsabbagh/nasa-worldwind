7018bd8286d2113ac4f081735027dc1d18250a87
mevers <mevers@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2015-07-22 01:34:32 +0000

    Natural Language search is now built dynamically with JQuery. Input boxes close when the user opens another input box. The page automatically loads WW when all fields are filled. The NLForm is now a module.

---
+++ WebWorldWind/apps/NaturalLanguageForm/js/nlbuilder.js
@@ -0,0 +1,30 @@
+
+//Needs a form with id='nl-form' in html
+define(
+    function(){
+
+        function NLBuilder ( el ) {
+            this.el = el
+        }
+
+        NLBuilder.prototype.addBasicText = function (text) {
+            this.el.append(text)
+        };
+        /*
+        @param ex: the word that appears on click and as default
+        @param forex: subline string e.g. "For example: <em>cafe</em> or <em>Fad</em>"
+         */
+        NLBuilder.prototype.addField = function (id, placeholder, forex) {
+            var field = $('<input>');
+                //<input id="amenityField" type="text" value="" placeholder="amenity" data-subline="For example: <em>cafe</em>">
+            field.attr('id', id);
+            field.attr('type', 'text');
+            field.attr('value', '');
+            field.attr('placeholder', placeholder);
+            field.attr('data-subline', forex);
+            this.el.append(field)
+        };
+
+        return NLBuilder
+    }
+);
