411d24c99edb72f029133cb61dccf7fbb4c958a8
mevers <mevers@822214bb-dcf7-4c8d-a767-17ae0fb20d88>
2015-07-30 02:15:57 +0000

    Multiple apps can now be tethered to the nlcanvas

--- trunk/WebWorldWind/apps/NaturalLanguageForm/js/nlform.js
+++ trunk/WebWorldWind/apps/NaturalLanguageForm/js/nlform.js
@@ -18,22 +18,98 @@
 * @dependency: This module requires a 'background' in the html under <body class="nl-blurred">
 *
 * @param window: the document's window.
+* @param arrayofforms: an array of natural language forms created in the format returned by the nlbuilder.
  */
 
-define(['OpenStreetMapApp'],
-	function (OpenStreetMapApp){
+define(['OpenStreetMapApp', 'WorldWindBase'],
+	function (OpenStreetMapApp, WorldWindBase){
 		'use strict';
-		function NaturalLanguageCanvas ( window ) {
-			var document = window.document;
+		function NaturalLanguageCanvas ( window , arrayofforms) {
+			this.document = window.document;
+			this.jQueryDoc = $(window.document);
+			this.forms = [];
 
 			if (!String.prototype.trim) {
 				String.prototype.trim=function(){return this.replace(/^\s+|\s+$/g, '');};
 			}
 
-			function NLForm( el ) {
-				var self = this;
+			this.buildForms(arrayofforms)
+
+		}
+
+		NaturalLanguageCanvas.prototype.buildForms = function (arrayofforms) {
+			var self = this;
+			console.log(arrayofforms);
+			arrayofforms.forEach(function(form){
+				var htmlForm = form.getForm();
+				$('.main-clearfix').append(htmlForm);
+				var canvasForm = new (self.NLForm()) ( self.document.getElementById(htmlForm.attr('id')) );
+				canvasForm._setApplicationToOpen(form.application);
+				self.forms.push(canvasForm)
+			})
+
+		};
+
+		NaturalLanguageCanvas.prototype._loadPageIfAllFormsFilled = function () {
+			var allFormsFilled = true;
+			this.forms.forEach(function (form) {
+
+				if (!form.hasAllFields){
+					allFormsFilled = false
+				}
+
+			});
+
+			if (allFormsFilled){
+				this._loadPage()
+			}
+		};
+
+		NaturalLanguageCanvas.prototype._loadPage = function () {
+			var self = this
+			/*
+			 * Initialize the loading sequence of events.
+			 * The pin icon follows the path of a x^6 function. To make it steeper or less so,
+			 * raise/lower the power.
+			 */
+			var loadingScreen = function(){
+				var x = 0, y = 0, m = (self.jQueryDoc.height()/2 - 75)/Math.pow((self.jQueryDoc.width()/2 - 90),6);
+				var INDEX = 0,
+					steps = 100,
+					loadIcon = $('#apDiv2'),
+					loadSymbol = $('#apDiv3');
+				var loadTimeAnimator = window.setInterval(function () {
+					loadIcon.remove();
+					if (INDEX < steps){
+						INDEX += 1;
+					} else {
+						loadSymbol.fadeOut(4000, function(){
+							$('#landingScreen').fadeOut(400)//.remove();
+							//$('body').removeClass('nl-blurred');
+							//if (window.NLForm){
+							//	delete window.NLForm
+							//}
+							loadSymbol.css('right', 78);
+							loadSymbol.css('bottom',  22);
+						})
+					}
+					x = ((self.jQueryDoc.width()/2 - 78)/steps)*INDEX;
+					y = m*Math.pow(x,6);
+					loadSymbol.css('right', x + 78);
+					loadSymbol.css('bottom',  y + 22);
+				},20);
+			}();
+
+		};
+
+		NaturalLanguageCanvas.prototype.NLForm = function () {
+			var self = this;
+			function nlform( el ) {
+				var form = this;
+				this.canvas = self;
 				this.document = $(document);
 				this.el = el;
+				console.log(this.el);
 				this.backDrop = document.querySelector( '.nl-blurred' );
 				this.fields = [];
 				this.fldOpen = -1;
@@ -41,78 +117,51 @@ define(['OpenStreetMapApp'],
 				this._init();
 			}
 
-			NLForm.prototype = {
+			nlform.prototype = {
 				_init : function() {
-					var self = this;
-					Array.prototype.slice.call( this.el.querySelectorAll( 'select' ) ).forEach( function( el, i ) {
-						self.fields.push( new NLField( self, el, 'dropdown', i ) );
+					var form = this;
+					Array.prototype.slice.call( form.el.querySelectorAll( 'select' ) ).forEach( function( el, i ) {
+						form.fields.push( new (self.NLField())( form, el, 'dropdown', i ) );
 					} );
-					Array.prototype.slice.call( this.el.querySelectorAll( 'input' ) ).forEach( function( el, i ) {
-						self.fields.push( new NLField( self, el, 'input', i ) );
+					Array.prototype.slice.call( form.el.querySelectorAll( 'input' ) ).forEach( function( el, i ) {
+						form.fields.push( new (self.NLField())( form, el, 'input', i ) );
 					} );
 					//Close the boxes if the user clicks elsewhere
-					this.backDrop.addEventListener( 'click', function(ev) { ev.preventDefault(); ev.stopPropagation(); self.closeAllFields(); } );
-					this.backDrop.addEventListener( 'touchstart', function(ev) { ev.preventDefault(); ev.stopPropagation(); self.closeAllFields(); } );
+					this.backDrop.addEventListener( 'click', function(ev) { ev.preventDefault(); ev.stopPropagation(); form.closeAllFields(); } );
+					this.backDrop.addEventListener( 'touchstart', function(ev) { ev.preventDefault(); ev.stopPropagation(); form.closeAllFields(); } );
 				},
-				_loadPage : function() {
+				_loadLayer : function() {
 
-					var self = this;
+					var form = this;
 					/*
-					* Start the World Wind window in the background so that it can load the tiles.
-					*/
-					console.log(OpenStreetMapApp)
+					 * Start the World Wind window in the background so that it can load the tiles.
+					 */
+					//console.log(OpenStreetMapApp)
 					var loadInBackground = function(){
 						var amenity = $('#amenityField').val().trim();
 						var address = $('#addressField').val().trim();
-						var newBody = $('<canvas>');
-						newBody.attr('id', 'globe');
-						$('body').append(newBody);
-						console.log('amenity ', amenity);
-						console.log('address ', address );
-						new OpenStreetMapApp(amenity, address);
+
+						//console.log('amenity ', amenity);
+						//console.log('address ', address );
+						if (!window.worldWindow) {
+							window.worldWindow = new WorldWindBase( window )
+						}
+						new form.application(window.worldWindow, amenity, address);
 					}();
 
 					//Remove the natural language line because the user is done with it.
 					this.el.remove();
-
-					/*
-					* Initialize the loading sequence of events.
-					* The pin icon follows the path of a x^6 function. To make it steeper or less so,
-					* raise/lower the power.
-					*/
-					var loadingScreen = function(){
-						var x = 0, y = 0, m = (self.document.height()/2 - 75)/Math.pow((self.document.width()/2 - 90),6);
-						var INDEX = 0,
-							steps = 100,
-							loadIcon = $('#apDiv2'),
-							loadSymbol = $('#apDiv3');
-						var loadTimeAnimator = window.setInterval(function () {
-							loadIcon.remove();
-							if (INDEX < steps){
-								INDEX += 1;
-							} else {
-								loadSymbol.fadeOut(4000, function(){
-									$('#landingScreen').remove();
-									$('body').removeClass('nl-blurred');
-									if (window.NLForm){
-										delete window.NLForm
-									}
-								})
-							}
-							x = ((self.document.width()/2 - 78)/steps)*INDEX;
-							y = m*Math.pow(x,6);
-							loadSymbol.css('right', x + 78);
-							loadSymbol.css('bottom',  y + 22);
-						},20);
-					}();
-
+					this.canvas._loadPageIfAllFormsFilled();
+				},
+				_setApplicationToOpen: function(application) {
+					this.application = application;
 				},
 				/*
-				* Loops through all the fields and checks if they have an entry. If at least one does not have
-				* an entry, return false, else true.
-				*
-				* @ return: False if <aboveCondition> else True
-				*/
+				 * Loops through all the fields and checks if they have an entry. If at least one does not have
+				 * an entry, return false, else true.
+				 *
+				 * @ return: False if <aboveCondition> else True
+				 */
 				_areAllFieldsFilled : function() {
 					var self = this;
 					self.hasAllFields = true;
@@ -131,8 +180,8 @@ define(['OpenStreetMapApp'],
 
 				},
 				/*
-				* Loops through each field and closes them.
-				*/
+				 * Loops through each field and closes them.
+				 */
 				closeAllFields : function () {
 					var self = this;
 					self.fields.forEach(function(field){
@@ -142,13 +191,19 @@ define(['OpenStreetMapApp'],
 				},
 				runWorldWindIfAllEntriesFilled : function () {
 					if (this._areAllFieldsFilled()){
-						this._loadPage();
+						this._loadLayer();
 					}
 				}
 			};
 
-			function NLField( form, el, type, idx ) {
-				var self = this;
+			return nlform
+		};
+
+		NaturalLanguageCanvas.prototype.NLField = function( ) {
+			var self = this
+			function nlfield( form, el, type, idx ) {
+				var field = this;
+				this.canvas = self
 				this.form = form;
 				this.elOriginal = el;
 				this.pos = idx;
@@ -157,7 +212,7 @@ define(['OpenStreetMapApp'],
 				this._initEvents();
 			}
 
-			NLField.prototype = {
+			nlfield.prototype = {
 				_create : function() {
 					if( this.type === 'dropdown' ) {
 						this._createDropDown();
@@ -167,7 +222,7 @@ define(['OpenStreetMapApp'],
 					}
 				},
 				_createDropDown : function() {
-					var self = this;
+					var field = this;
 					this.hasUserEntry = false;
 					this.fld = document.createElement( 'div' );
 					this.fld.className = 'nl-field nl-dd';
@@ -179,8 +234,8 @@ define(['OpenStreetMapApp'],
 					Array.prototype.slice.call( this.elOriginal.querySelectorAll( 'option' ) ).forEach( function( el, i ) {
 						ihtml += self.elOriginal.selectedIndex === i ? '<li class="nl-dd-checked">' + el.innerHTML + '</li>' : '<li>' + el.innerHTML + '</li>';
 						// selected index value
-						if( self.elOriginal.selectedIndex === i ) {
-							self.selectedIdx = i;
+						if( field.elOriginal.selectedIndex === i ) {
+							field.selectedIdx = i;
 						}
 					} );
 					this.optionsList.innerHTML = ihtml;
@@ -190,47 +245,8 @@ define(['OpenStreetMapApp'],
 					this.elOriginal.style.display = 'none';
 				},
 				_createInput : function() {
-					var self = this;
+					var field = this;
 					this.hasUserEntry = false;
-					////////////////////////////////////
-					// JQUERY isn't worth it here.
-					//this.toggle = $( '<a>' );
-					//this.toggle.append( this.elOriginal.getAttribute( 'placeholder' ) );
-					//this.toggle.attr('class', 'nl-field-toggle');
-                    //
-					//this.getinput = $('<input>');
-					//this.getinput.attr('type', 'text');
-					//this.getinput.attr('placeholder', this.elOriginal.getAttribute( 'placeholder' ));
-                    //
-					//this.getinputWrapper = $('<li>');
-					//this.getinputWrapper.attr('class', 'nl-ti-input');
-                    //
-					//this.inputsubmit = $('<button>'); //document.createElement( 'button' );
-					//this.inputsubmit.attr('class', 'nl-field-go');
-					//this.inputsubmit.append('Go');
-                    //
-					//this.getinputWrapper.append(this.getinput);
-					//this.getinputWrapper.append(this.inputsubmit);
-                    //
-					//this.example = $('<li>');
-					//this.example.attr('class', 'nl-ti-example');
-					//this.example.append( this.elOriginal.getAttribute( 'data-subline' ) );
-                    //
-					//this.optionsList = $('<ul>');
-					//this.optionsList.append( this.getinputWrapper );
-					//this.optionsList.append( this.example );
-                    //
-					//this.fld = $('<div>');
-					//this.fld.attr('class', 'nl-field nl-ti-text');
-					//this.fld.append( this.toggle );
-					//this.fld.append( this.optionsList);
-                    //
-					//this.elOriginal.attr('style', 'display: none;')
-					//this.elOriginal.parent().insertBefore( this.fld, this.elOriginal );
-					//console.log(this.elOriginal)
-					//this.elOriginal.parentNode.insertBefore( this.fld, this.elOriginal );
-					//this.elOriginal.style.display = 'none';
-					////////////////////////////////////////////////////////////////////////////
 					this.fld = document.createElement( 'div' );
 					this.fld.className = 'nl-field nl-ti-text';
 					this.toggle = document.createElement( 'a' );
@@ -265,25 +281,25 @@ define(['OpenStreetMapApp'],
 					//this.fld
 				},
 				_initEvents : function() {
-					var self = this;
-					this.toggle.addEventListener( 'click', function( ev ) { ev.preventDefault(); ev.stopPropagation(); self._open(); } );
-					this.toggle.addEventListener( 'touchstart', function( ev ) { ev.preventDefault(); ev.stopPropagation(); self._open(); } );
+					var field = this;
+					this.toggle.addEventListener( 'click', function( ev ) { ev.preventDefault(); ev.stopPropagation(); field._open(); } );
+					this.toggle.addEventListener( 'touchstart', function( ev ) { ev.preventDefault(); ev.stopPropagation(); field._open(); } );
 
 					if( this.type === 'dropdown' ) {
 						var opts = Array.prototype.slice.call( this.optionsList.querySelectorAll( 'li' ) );
 						opts.forEach( function( el, i ) {
-							el.addEventListener( 'click', function( ev ) { ev.preventDefault(); ev.stopPropagation(); self.close( el, opts.indexOf( el ) ); } );
-							el.addEventListener( 'touchstart', function( ev ) { ev.preventDefault(); ev.stopPropagation(); self.close( el, opts.indexOf( el ) ); } );
+							el.addEventListener( 'click', function( ev ) { ev.preventDefault(); ev.stopPropagation(); field.close( el, opts.indexOf( el ) ); } );
+							el.addEventListener( 'touchstart', function( ev ) { ev.preventDefault(); ev.stopPropagation(); field.close( el, opts.indexOf( el ) ); } );
 						} );
 					}
 					else if( this.type === 'input' ) {
 						this.getinput.addEventListener( 'keydown', function( ev ) {
 							if ( ev.keyCode == 13 ) {
-								self.close();
+								field.close();
 							}
 						} );
-						this.inputsubmit.addEventListener( 'click', function( ev ) { ev.preventDefault(); ev.stopPropagation(); self.close();} );
-						this.inputsubmit.addEventListener( 'touchstart', function( ev ) { ev.preventDefault(); ev.stopPropagation(); self.close(); } );
+						this.inputsubmit.addEventListener( 'click', function( ev ) { ev.preventDefault(); ev.stopPropagation(); field.close();} );
+						this.inputsubmit.addEventListener( 'touchstart', function( ev ) { ev.preventDefault(); ev.stopPropagation(); field.close(); } );
 					}
 
 				},
@@ -302,7 +318,7 @@ define(['OpenStreetMapApp'],
 					this.getinput.focus()
 				},
 				close : function( opt, idx ) {
-					var self = this;
+					var field = this;
 					if( !this.open ) {
 						return false;
 					}
@@ -325,11 +341,11 @@ define(['OpenStreetMapApp'],
 					else if( this.type === 'input' ) {
 						this.getinput.blur();
 						if (this.getinput.value.trim() !== ''){
-							self.hasUserEntry = true;
-							self.toggle.innerHTML = self.getinput.value;
+							field.hasUserEntry = true;
+							field.toggle.innerHTML = field.getinput.value;
 						} else {
-							self.hasUserEntry = false;
-							self.toggle.innerHTML = self.getinput.getAttribute( 'placeholder' )
+							field.hasUserEntry = false;
+							field.toggle.innerHTML = field.getinput.getAttribute( 'placeholder' )
 						}
 						this.elOriginal.value = this.getinput.value;
 					}
@@ -343,10 +359,9 @@ define(['OpenStreetMapApp'],
 				}
 			};
 
-			// add to global namespace
-			console.log('load to global')
-			window.NLForm = NLForm;
-		}
+			return nlfield
+		};
+
 
 		return NaturalLanguageCanvas
 	});
